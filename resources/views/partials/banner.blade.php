6062da9aab5ef75f4d6a98daa0a371d4207467bd
Jael Atam <jael.atam@latitudesud.gp>
2021-02-11 16:24:44 -0400

    fix header logo link

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,10 +1,10 @@
 <div data-collapse="medium" data-animation="default" data-duration="400" role="banner" class="nav w-nav">
     <div class="nav__container">
         <div class="nav_conrainer__wrapper">
-            <a data-w-id="Link Block 11" href="https://www.carrefour-martinique.com/"
+            <a data-w-id="Link Block 11" href="https://www.carrefour-martinique.com/" rel="nofollow"
                class="logo-full w-inline-block"><img src="images/logo-jai_choisi_color.svg" loading="lazy"
                                                      height="60" width="150" alt="" class="logo"></a>
-            <a href="#" class="logo-short w-inline-block"><img src="images/logo-short.svg" loading="lazy" width="50"
+            <a href="https://www.carrefour-martinique.com/" rel="nofollow" class="logo-short w-inline-block"><img src="images/logo-short.svg" loading="lazy" width="50"
                                                                alt=""></a>
             <a href="{{url('/')}}" class="event-link w-nav-brand">
                 <h1 class="event">Avec Carrefour, <span class="text-span-4">je choisis le meilleur pour bébé</span>
